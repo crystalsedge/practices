75d12c328de8ee08acd18c0289dcc86e7923a38d
crystalsedge <viindubs@gmail.com>
2022-12-19 14:00:20 +0000

    capacitor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PreviewAnyFile } from '@ionic-native/preview-any-file';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    PreviewAnyFile
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
